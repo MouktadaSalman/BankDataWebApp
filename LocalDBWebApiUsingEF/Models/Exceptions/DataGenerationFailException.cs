73a807de7368ca8f2a57429e39e3564955cb8e09
MJQ <21494299@student.curtin.edu.au>
2024-10-02 16:13:47 +0800

    Add custom exceptions

---
+++ LocalDBWebApiUsingEF/Models/Exceptions/DataGenerationFailException.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection.Metadata;
+
+namespace DataTierWebServer.Models.Exceptions
+{
+    public class DataGenerationFailException : Exception
+    {
+        private const string BaseMessage = "Failed to generate entry data for: ";
+        public DataGenerationFailException(string database) : base(BaseMessage + database) { }
+
+        public DataGenerationFailException(string database, Exception innerException) : base(BaseMessage + database, innerException) { }
+    }
+}
---
+++ LocalDBWebApiUsingEF/Models/Exceptions/MissingProfileException.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualBasic;
+
+namespace DataTierWebServer.Models.Exceptions
+{
+    public class MissingProfileException : Exception
+    {
+        private const string BaseMessage = "User profile does not exist for requested: ";
+        public MissingProfileException(string name) : base(BaseMessage + name) { }
+
+        public MissingProfileException(string name, Exception innerException) : base(BaseMessage + name, innerException) { }
+    }
+}
