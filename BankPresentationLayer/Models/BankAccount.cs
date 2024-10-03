51eefddd8a2f5d0c367078fb808890d203657292
MJQ <21494299@student.curtin.edu.au>
2024-10-03 09:55:42 +0800

    Attempt to fix conflicts

--- BankPresentationLayer/Models/BankAccount.cs
+++ BankPresentationLayer/Models/BankAccount.cs
@@ -9,16 +9,20 @@ namespace BankPresentationLayer.Models
     public class BankAccount
     {
         public uint AcctNo { get; set; }
-        public string AccountName { get; set; }
+        public string? AccountName { get; set; }
         public int Balance { get; set; }
         public int UserId { get; set; }
-        public ICollection<UserHistory> History { get; set; }
+        public ICollection<UserHistory>? History { get; set; }
     }
 
     public class UserHistory
     {
         public int Transaction { get; set; }
         public uint AccountId { get; set; }
-        public string HistoryString { get; set; }
+        public double Amount { get; set; }
+        public string? Type { get; set; }
+        public DateTime DateTime { get; set; }
+        public uint Sender { get; set; }
+        public string? HistoryString { get; set; }
     }
 }
